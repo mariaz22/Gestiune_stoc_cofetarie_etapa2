fc1c947812128adeb290615d710d31dc887bf51c
mariaz22 <maria.zidaroiu22@yahoo.com>
2025-06-05 19:30:19 +0300

    Update DatabaseConnection.java

--- src/service/DatabaseConnection.java
+++ src/service/DatabaseConnection.java
@@ -16,7 +16,7 @@ public class DatabaseConnection {
         try {
             Class.forName("org.postgresql.Driver");
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Conectat cu succes la baza de date PostgreSQL.");
+            System.out.println("Conectat la baza de date");
         } catch (SQLException | ClassNotFoundException e) {
             System.out.println("Eroare la conectarea la baza de date: " + e.getMessage());
         }
